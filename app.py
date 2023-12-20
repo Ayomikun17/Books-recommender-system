d4b6c4f9787c1ea52de7e5eb0efcd6ddd5b85a68
Ayomikun17 <90663827+Ayomikun17@users.noreply.github.com>
2023-12-20 23:15:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,16 @@
 import pickle
-import streamlit as st 
+import streamlit as st
 import numpy as np
 
 
-st.header('Books Recommender System')
-try:
-    with open('artifacts/model.pkl', 'rb') as file:
-        model = pickle.load(file)
-except Exception as e:
-    print("Error loading the pickled model:", e)
+st.header('Book Recommender System Using Machine Learning')
+model = pickle.load(open('artifacts/model.pkl','rb'))
+book_names = pickle.load(open('artifacts/book_names.pkl','rb'))
+final_rating = pickle.load(open('artifacts/final_rating.pkl','rb'))
+book_pivot = pickle.load(open('artifacts/book_pivot.pkl','rb'))
 
-try:
-    with open('artifacts/books_name.pkl', 'rb') as file:
-        books_name = pickle.load(file)
-except Exception as e:
-    print("Error loading 'books_name.pkl':", e)
-
-try:
-    with open('artifacts/final_rating.pkl', 'rb') as file:
-        final_rating = pickle.load(file)
-except Exception as e:
-    print("Error loading 'final_rating.pkl':", e)
-
-try:
-    with open('artifacts/book_pivot.pkl', 'rb') as file:
-        book_pivot = pickle.load(file)
-except Exception as e:
-    print("Error loading 'book_pivot.pkl':", e)
 
 def fetch_poster(suggestion):
-    #Getting the name, index and image url of the book
     book_name = []
     ids_index = []
     poster_url = []
@@ -37,58 +18,54 @@ def fetch_poster(suggestion):
     for book_id in suggestion:
         book_name.append(book_pivot.index[book_id])
 
-    for name in book_name[0]:
-        ids = np.where(final_rating['title']== name)[0][0]
+    for name in book_name[0]: 
+        ids = np.where(final_rating['title'] == name)[0][0]
         ids_index.append(ids)
 
-    for ids in ids_index:
-        url = final_rating.iloc[ids]['img_url']
+    for idx in ids_index:
+        url = final_rating.iloc[idx]['image_url']
         poster_url.append(url)
+
     return poster_url
 
+
+
 def recommend_book(book_name):
-    global book_pivot
-    book_list =[]
+    books_list = []
     book_id = np.where(book_pivot.index == book_name)[0][0]
-    distance,suggestion = model.kneighbors(book_pivot.iloc[book_id,:].values.reshape(1,-1), n_neighbors = 6)
-    
+    distance, suggestion = model.kneighbors(book_pivot.iloc[book_id,:].values.reshape(1,-1), n_neighbors=6 )
+
     poster_url = fetch_poster(suggestion)
     
-    for i in range (len(suggestion)):
-        books = book_pivot.index[suggestion[i]]
-        for j in books:
-            book_list.append(j)
-    return book_list,poster_url
+    for i in range(len(suggestion)):
+            books = book_pivot.index[suggestion[i]]
+            for j in books:
+                books_list.append(j)
+    return books_list , poster_url       
 
 
-selected_books = st.selectbox('Type or select a book', books_name)
+
+selected_books = st.selectbox(
+    "Type or select a book from the dropdown",
+    book_names
+)
 
 if st.button('Show Recommendation'):
-    recommendation_books, poster_url = recommend_book(selected_books, book_pivot)
+    recommended_books,poster_url = recommend_book(selected_books)
     col1, col2, col3, col4, col5 = st.columns(5)
-
-
     with col1:
-        # Didn't start with index 0 because it returns the entered book's name
-        st.text(recommendation_books[1])
-        try:
-            st.image(poster_url[1])
-        except Exception as e:
-            st.error(f"Error loading image: {e}")
-
+        st.text(recommended_books[1])
+        st.image(poster_url[1])
     with col2:
-        st.text(recommendation_books[2])
+        st.text(recommended_books[2])
         st.image(poster_url[2])
 
     with col3:
-        st.text(recommendation_books[3])
+        st.text(recommended_books[3])
         st.image(poster_url[3])
-
     with col4:
-        st.text(recommendation_books[4])
+        st.text(recommended_books[4])
         st.image(poster_url[4])
-
     with col5:
-        st.text(recommendation_books[5])
+        st.text(recommended_books[5])
         st.image(poster_url[5])
-
