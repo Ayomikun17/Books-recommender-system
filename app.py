15e122dd17102278c435b41884eb8bc2b24588aa
Ayomikun17 <90663827+Ayomikun17@users.noreply.github.com>
2023-12-20 22:09:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,54 +3,76 @@ import streamlit as st
 import numpy as np
 
 st.header('Books Recommender System')
-model = pickle.load(open('artifacts/model.pkl','rb'))
-books_name = pickle.load(open('artifacts/books_name.pkl','rb'))
-final_rating = pickle.load(open('artifacts/final_rating.pkl','rb'))
-book_pivot = pickle.load(open('artifacts/book_pivot.pkl','rb')) 
+try:
+    with open('artifacts/model.pkl', 'rb') as file:
+        model = pickle.load(file)
+except Exception as e:
+    print("Error loading the pickled model:", e)
+    
+try:
+    with open('artifacts/books_name.pkl', 'rb') as file:
+        books_name = pickle.load(file)
+except Exception as e:
+    print("Error loading 'books_name.pkl':", e)
+
+try:
+    with open('artifacts/final_rating.pkl', 'rb') as file:
+        final_rating = pickle.load(file)
+except Exception as e:
+    print("Error loading 'final_rating.pkl':", e)
+
+try:
+    with open('artifacts/book_pivot.pkl', 'rb') as file:
+        book_pivot = pickle.load(file)
+except Exception as e:
+    print("Error loading 'book_pivot.pkl':", e)
 
 def fetch_poster(suggestion):
-    book_name =[]
+    #Getting the name, index and image url of the book
+    book_name = []
     ids_index = []
-    poster_url =[]
+    poster_url = []
+
     for book_id in suggestion:
         book_name.append(book_pivot.index[book_id])
 
     for name in book_name[0]:
-    ids= np.where(final_rating['title'] == name)[0][0]
-    ids_index.append(ids)
+        ids = np.where(final_rating['title']== name)[0][0]
+        ids_index.append(ids)
 
-    for idx in ids_index:
-        url = final_rating.iloc[idx]['img_url']
+    for ids in ids_index:
+        url = final_rating.iloc[ids]['img_url']
         poster_url.append(url)
-
     return poster_url
 
-def recommend_books(book_name):
-    book_list = []
+def recommend_book(book_name):
+    book_list =[]
     book_id = np.where(book_pivot.index == book_name)[0][0]
-    distance,suggestion = model.kneighbors(book_pivot.iloc[book_isd,:].values.reshape(1,-1), n_neighbors=6)
-
+    distance,suggestion = model.kneighbors(book_pivot.iloc[book_id,:].values.reshape(1,-1), n_neighbors = 6)
+    
     poster_url = fetch_poster(suggestion)
-
+    
     for i in range (len(suggestion)):
-        books= book_pivot.index[suggestion[i]]
-
+        books = book_pivot.index[suggestion[i]]
         for j in books:
             book_list.append(j)
-
-    return book_list, poster_url
-
+    return book_list,poster_url
 
 
 selected_books = st.selectbox('Type or select a book', books_name)
 
 if st.button('Show Recommendation'):
-    recommendation_books, poster_url = recommend_books(selected_books)
-    col1,col2,col3,col4,col5 = st.columns(5)
+    recommendation_books, poster_url = recommend_book(selected_books)
+    col1, col2, col3, col4, col5 = st.columns(5)
+
 
     with col1:
+        # Didn't start with index 0 because it returns the entered book's name
         st.text(recommendation_books[1])
-        st.image(poster_url[1])
+        try:
+            st.image(poster_url[1])
+        except Exception as e:
+            st.error(f"Error loading image: {e}")
 
     with col2:
         st.text(recommendation_books[2])
@@ -67,7 +89,6 @@ if st.button('Show Recommendation'):
     with col5:
         st.text(recommendation_books[5])
         st.image(poster_url[5])
-
 
 
 
